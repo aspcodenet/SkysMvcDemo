3e5cd0b6917040af7e3dbf3ef435d739f495fdbb
Stefan Holmberg <stefan.holmberg@systementor.se>
2022-01-20 10:25:09 +0100

    a

--- SkysMvcDemo/Controllers/ProductController.cs
+++ SkysMvcDemo/Controllers/ProductController.cs
@@ -1,6 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Controllers;
 using SkysMvcDemo.Models;
+using SkysMvcDemo.ViewModels;
 
 namespace SkysMvcDemo.Controllers
 {
@@ -14,12 +17,24 @@ namespace SkysMvcDemo.Controllers
         }
         public IActionResult Index()
         {
-            return View();
+            List<ProductViewModel> viewModel = _productRepository.GetAll()
+                .Select(p => new ProductViewModel
+                {
+                    Id = p.Id,
+                    Name = p.Name,
+                    Price = p.Price,
+                }).ToList();
+            return View(viewModel);
         }
 
         public IActionResult ShowOne(int bajskorv)
         {
-            return View();
+            var product = _productRepository.GetAll().First(e => e.Id == bajskorv);
+            var viewModel = new ProductViewModel();
+            viewModel.Name = product.Name;
+            viewModel.Price = product.Price;
+            viewModel.Id = product.Id;
+            return View(viewModel);
         }
 
     }
