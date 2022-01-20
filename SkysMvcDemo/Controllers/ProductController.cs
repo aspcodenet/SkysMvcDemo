64ed4bdf3e111e07a9091de853ee19d05d973520
Stefan Holmberg <stefan.holmberg@systementor.se>
2022-01-20 09:37:07 +0100

    Add project files.

---
+++ SkysMvcDemo/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using SkysMvcDemo.Models;
+
+namespace SkysMvcDemo.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductRepository _productRepository;
+
+        public ProductController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ShowOne(int bajskorv)
+        {
+            return View();
+        }
+
+    }
+}
